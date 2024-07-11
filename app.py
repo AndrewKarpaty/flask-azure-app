e3ad5673501006a842960b6e032c4b2d8571b528
Andriy Striletskyi <yaremche96@ukr.net>
2024-07-11 20:01:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os   
 from azure.storage.blob import BlobServiceClient
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template_string
 
 app = Flask(__name__)  
 
 connect_str = os.getenv('AZURE_STORAGEBLOB_CONNECTIONSTRING') # retrieve the connection string from the environment variable
-container_name = "cp-images" # container name in which images will be store in the storage account
+container_name = "cp-images" # container name in which images will be stored in the storage account
 
 blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str) # create a blob service client to interact with the storage account
 
@@ -16,21 +16,9 @@ except Exception as e:
     container_client = blob_service_client.create_container(container_name) # create a container in the storage account if it does not exist
 
 @app.route("/")
-def view_photos():
-    blob_items = container_client.list_blobs() # list all the blobs in the container
-
-    img_html = "<div style='display: flex; justify-content: space-between; flex-wrap: wrap;'>"
-
-    for blob in blob_items:
-        blob_client = container_client.get_blob_client(blob=blob.name) # get blob client to interact with the blob and get blob url
-        img_html += "<img src='{}' width='auto' height='200' style='margin: 0.5em 0;'/>".format(blob_client.url) # get the blob url and append it to the html
-    
-    img_html += "</div>"
-
-    # return the html with the images
-    return """
+def home():
+    return render_template_string("""
     <head>
-    <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
@@ -43,8 +31,7 @@ def view_photos():
             <div class="card" style="margin: 1em 0; padding: 1em 0 0 0; align-items: center;">
                 <h3>Upload new File</h3>
                 <div class="form-group">
-                    <form method="post" action="/upload-photos" 
-                        enctype="multipart/form-data">
+                    <form method="post" action="/upload-photos" enctype="multipart/form-data">
                         <div style="display: flex;">
                             <input type="file" accept=".png, .jpeg, .jpg, .gif" name="photos" multiple class="form-control" style="margin-right: 1em;">
                             <input type="submit" class="btn btn-primary">
@@ -52,10 +39,46 @@ def view_photos():
                     </form>
                 </div> 
             </div>
-        
-    """ + img_html + "</div></body>"
+            <div style="text-align: center;">
+                <a href="/view-photos" class="btn btn-success">View Photos</a>
+            </div>
+        </div>
+    </body>
+    """)
 
-#flask endpoint to upload a photo  
+@app.route("/view-photos")
+def view_photos():
+    blob_items = container_client.list_blobs() # list all the blobs in the container
+
+    img_html = "<div style='display: flex; justify-content: space-between; flex-wrap: wrap;'>"
+
+    for blob in blob_items:
+        blob_client = container_client.get_blob_client(blob=blob.name) # get blob client to interact with the blob and get blob url
+        img_html += "<img src='{}' width='auto' height='200' style='margin: 0.5em 0;'/>".format(blob_client.url) # get the blob url and append it to the html
+    
+    img_html += "</div>"
+
+    return render_template_string("""
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="/">Photos App</a>
+            </div>
+        </nav>
+        <div class="container">
+            <div style="margin: 1em 0; padding: 1em 0; align-items: center;">
+                <h3>Uploaded Photos</h3>
+                <a href="/" class="btn btn-primary">Upload More Photos</a>
+            </div>
+            {{ img_html|safe }}
+        </div>
+    </body>
+    """, img_html=img_html)
+
+# flask endpoint to upload a photo  
 @app.route("/upload-photos", methods=["POST"])
 def upload_photos():
     filenames = ""
@@ -68,4 +91,7 @@ def upload_photos():
             print(e)
             print("Ignoring duplicate filenames") # ignore duplicate filenames
         
-    return redirect('/')  
+    return redirect('/view-photos')  
+
+if __name__ == "__main__":
+    app.run(debug=True)
